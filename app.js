b2fcf498bc8faf282d95b38c448675e8d79ce9c8
Loïc DELAUNAY <loicdelaunay05@gmail.com>
2018-03-13 19:18:08 +0100

    0.1
    - test de composants

--- app.js
+++ app.js
@@ -4,43 +4,34 @@
 
 //Chargement du module de sous process
 const { exec } = require('child_process');
-const defaults = {
-    encoding: 'utf8',
-    timeout: 0,
-    killSignal: 'SIGTERM',
-    cwd: null,
-    env: null
-};
 
 //Chargement du module de cron
 const Cron = require('cron').CronJob;
 
-//Option
+//Options
 const creanauStart = 1;
 const creanauEnd = 16;
+const streamlinkexec = "C:/Users/Dream/Desktop/Projets/SARS---Serveur-auto-record-stream/lib/Streamlink/bin/streamlink.exe";
 
 //Tableau test
 var test = ["test1","test2","test3"];
-var listProcess = [];
+var listeProcess = [];
 
 //Cron de vérification
-// new Cron('* * * * * *',function () {
-//     console.log("test");
-// },null,true);
+new Cron('* * * * * 1',function () {
+    console.log("test");
+},null,true);
 
 //Lance un bash
-function runFlux(){
+function runFlux(url,quality,file){
 //Execution d'un script avec controle de celui ci en async et promise
-    exec('test.sh', (err, stdout, stderr) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(stdout);
-    });
+    listeProcess.push(
+        exec(streamlinkexec + ' ' + url + ' ' + quality + ' ' + file, function(error, stdout, stderr) {
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        }))
 }
 
-runFlux();
-runFlux();
-runFlux();
-runFlux();
+
+runFlux('twitch.tv/ltzonda','720p','-o C:/Users/Dream/Desktop/Test/test.flv');
