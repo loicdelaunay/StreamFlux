68109e7eeed33aed0357258210473602703855c8
loïc delaunay <loicdelaunay05@gmail.com>
2018-06-11 19:07:55 +0200

    Modify : alert settings

--- libs/server-web/assets/js/include.js
+++ libs/server-web/assets/js/include.js
@@ -1,5 +1,5 @@
 // Material Select Initialization
-$(document).ready(function() {
+$(document).ready(function () {
     $('.mdb-select').material_select();
 });
 
@@ -10,6 +10,25 @@ $('#end-time').pickatime({});
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
+
+//Toastr settings
+toastr.options = {
+    "closeButton": true, // true/false
+    "debug": false, // true/false
+    "newestOnTop": true, // true/false
+    "progressBar": true, // true/false
+    "positionClass": "toast-bottom-right", // toast-top-right / toast-top-left / toast-bottom-right / toast-bottom-left
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300", // in milliseconds
+    "hideDuration": "1000", // in milliseconds
+    "timeOut": "5000", // in milliseconds
+    "extendedTimeOut": "1000", // in milliseconds
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
 
 //Socket.io connection
 const socket = io();
